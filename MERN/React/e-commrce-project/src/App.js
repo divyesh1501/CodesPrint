54daf5cc99d4f3525bb3b4231fde0447f074bc74
divyesh1501 <divyesh429.dp@gmail.com>
2024-06-11 18:35:49 +0530

    update

--- MERN/React/e-commrce-project/src/App.js
+++ MERN/React/e-commrce-project/src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom'
+import Home from './UI/pages/home/Home';
+import Order from './UI/pages/order/Order';
+import Cart from './UI/pages/cart/Cart';
+import Allproducts from './UI/pages/allproducts/Allproducts';
+import Nopage from './UI/pages/nopage/Nopage';
+import Dashboard from './UI/pages/admin/dashboard/Dashboard';
+import MyState from './UI/context/data/myState';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      <MyState>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/order' element={<Order />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/allproducts' element={<Allproducts />} />
+            <Route path='/#' element={<Nopage />} />
+          </Routes>
+        </Router>
+      </MyState>
+
     </>
   );
 }
---
+++ MERN/React/e-commrce-project/src/UI/pages/allproducts/Allproducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Allproducts() {
+  return (
+    <div>
+      Products
+    </div>
+  )
+}
+
+export default Allproducts
